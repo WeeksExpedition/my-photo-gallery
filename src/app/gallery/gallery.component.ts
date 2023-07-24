0dd45447b6df9a12f7b6440bf8a9c128fa62bd55
Lacey <lweekslaw@gmail.com>
2023-07-24 10:30:37 -0500

    gallery component started / photos need work

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.css']
+})
+export class GalleryComponent {
+
+  file = [
+    
+    "/assets/21.JPG",
+    "/assets/1.JPG",
+    "/assets/2.JPG",
+    "/assets/14.JPG",
+    "/assets/16.JPG",
+    "/assets/3.JPG",
+    "/assets/22.JPG",
+    "/assets/4.JPG",
+    "/assets/5.JPG",
+    "/assets/6.JPG",
+    "/assets/7.JPG",
+    "/assets/8.JPG",
+    "/assets/20.JPG",
+    "/assets/9.JPG",
+    "/assets/10.JPG",
+    "/assets/18.JPG",
+    "/assets/11.JPG",
+    "/assets/12.JPG",
+    "/assets/13.JPG",
+    "/assets/15.JPG",
+    "/assets/19.JPG",
+    "/assets/17.JPG",
+    "/assets/22.JPG"
+  ]
+
+}
