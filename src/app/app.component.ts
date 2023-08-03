6f715136166356a0ba6d5cb4d2dfdbfc5cce5363
Lacey <lweekslaw@gmail.com>
2023-08-03 09:56:23 -0500

    Currently reading from the data.json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent{
   title = 'my-photo-gallery';
-  galleryData:any;
-  url: string = '/assets/data.json';
-
-  constructor(private http: HttpClient) {}
-
-  ngOnInit() {
-    this.http.get(this.url).subscribe(res => {
-      this.galleryData = res;
-    });
-  }
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-gallery',
@@ -6,30 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./gallery.component.css']
 })
 
-export class GalleryComponent {
+export class GalleryComponent implements OnInit {
 
-  files = [
-    "/assets/21.JPG",
-    "/assets/1.JPG",
-    "/assets/2.JPG",
-    "/assets/14.JPG",
-    "/assets/16.JPG",
-    "/assets/3.JPG",
-    "/assets/22.JPG",
-    "/assets/4.JPG",
-    "/assets/5.JPG",
-    "/assets/6.JPG",
-    "/assets/7.JPG",
-    "/assets/8.JPG",
-    "/assets/20.JPG",
-    "/assets/9.JPG",
-    "/assets/10.JPG",
-    "/assets/18.JPG",
-    "/assets/11.JPG",
-    "/assets/12.JPG",
-    "/assets/13.JPG",
-    "/assets/15.JPG",
-    "/assets/19.JPG",
-    "/assets/17.JPG"
-  ]
+  title = 'my-photo-gallery';
+  galleryData:any;
+  url: string = '/assets/data.json';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(res => {
+      this.galleryData = res;
+    });
+  }
 }
