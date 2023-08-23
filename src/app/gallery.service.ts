073509cd955f0b9dfb66f0bee00fb7f88fa23ee8
Lacey <lweekslaw@gmail.com>
2023-08-23 14:15:43 -0500

    slight update

--- src/app/gallery.service.ts
+++ src/app/gallery.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { GalleryDto } from './gallery.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GalleryService {
 
+  url: string = 'assets/data.json';
+
+  constructor(private http:HttpClient) {}
+
+  getPhoto() {
+    return this.http.get<GalleryDto[]>(this.url);
+  }
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { GalleryDto } from '../gallery.interface';
 
 @Component({
   selector: 'app-gallery',
