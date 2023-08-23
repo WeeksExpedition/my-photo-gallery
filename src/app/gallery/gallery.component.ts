65d78bf2ed7aacad2ca7c287146429f5b25de6b6
Lacey <lweekslaw@gmail.com>
2023-08-23 13:52:17 -0500

    added interface and service / still needs work

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { GalleryDto } from '../gallery.interface';
 
 @Component({
   selector: 'app-gallery',
